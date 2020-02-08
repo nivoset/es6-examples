f4d5bef2289ce8d98ed7fbb370cc52f8f6246491
Ben Koop <nivoset@gmail.com>
2020-02-07 20:48:50 -0500

    more work

--- src/index.js
+++ src/index.js
@@ -13,8 +13,21 @@
 
 import './examples/filter_match'
 
+
+
 // import uuid from 'uuid';
 //import being used, as well as template literals
 // console.log(`new UUID is: ${uuid()}`);
 
 
+const readline = require("readline");
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Press any key to continue...", function (answer) {
+    rl.close();
+    process.exit(0);
+});
